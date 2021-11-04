0bcd626fda20d6e80aa9980490fec307287665f4
Alex <development@alexgidge.com>
2021-11-04 23:17:50 +0000

    spawn location

--- Assets/Characters/Player/PlayerMovement.cs
+++ Assets/Characters/Player/PlayerMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine.Serialization;
 public class PlayerMovement : CharacterMovement
 {
     public PlayerInput PlayerInput;
+    public Vector2 SpawnLocation = new Vector2(-4.5f, -12.5f);
    internal Quaternion currentRotation => transform.rotation;
     
     // Start is called before the first frame update
     void Start()
     {
         RegisterEvents();
+        transform.position = SpawnLocation;
     }
 
     private void RegisterEvents()
