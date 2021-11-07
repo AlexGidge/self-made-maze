b502e6328b6906b7d4ba480973eb64731f670564
Alex <development@alexgidge.com>
2021-11-07 17:20:13 +0000

    Small enhancements

--- Assets/Environment/EnvironmentController.cs
+++ Assets/Environment/EnvironmentController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnvironmentController : MonoBehaviour
@@ -8,7 +6,7 @@ public class EnvironmentController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(true);
+        imageToDisplayOnAwake.SetActive(true);
     }
 
     // Update is called once per frame
